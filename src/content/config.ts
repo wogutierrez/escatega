596e8f19fbf380100a0f94fe337e213e32fdde6e
Ortega <wjortega@bethel.jw.org>
2026-09-10 17:17:29 +0200

    all pages are up

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,4 @@
+// src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 
 const articlesCollection = defineCollection({
@@ -7,7 +8,11 @@ const articlesCollection = defineCollection({
     pubDate: z.coerce.date(),
     description: z.string(),
     coverImage: z.string().optional(),
-    category: z.enum(['Save Time & Stress','Protect Your Business','Get More Clients']),
+    category: z.enum([
+      'Save Time & Stress',
+      'Protect Your Business',
+      'Get More Clients',
+    ]),
     featured: z.boolean().default(false),
   }),
 });
